fea5140e5a10ae08495998ddc0ec5e5cf0aae500
Tejpex <equis_angel@hotmail.com>
2025-04-25 14:38:26 +0200

    Add placeholder hero img

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,6 +2,17 @@
 
 export const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div>
+      <img
+        src="https://images.unsplash.com/photo-1740031904427-ca6713523372?q=80&w=2920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Hero image"
+        style={{
+          width: "100%",
+          height: "300px",
+          objectFit: "cover",
+          objectPosition: "center",
+        }}
+      />
+    </div>
   )
 }
