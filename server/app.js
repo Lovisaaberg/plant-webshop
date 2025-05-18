db2debda2e9539f3a55ca86f5286f7fd55988ac4
Patric Angly <padde95@hotmail.se>
2025-05-18 21:15:28 +0200

    setup backend route for Stripe checkout

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// Middleware
+app.use(express.json());
+
+// Routes
+const checkoutRoutes = require("./routes/checkout");
+app.use("/api/stripe", checkoutRoutes);
+
+module.exports = app;
