af669bf29177d3b165a3d376281325c28a276693
Tejpex <equis_angel@hotmail.com>
2025-05-20 14:15:44 +0200

    Add contact-page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { LandingPage } from "./pages/LandingPage"
 import { ProductPage } from "./pages/ProductPage"
 import { CheckoutPage } from "./pages/CheckoutPage"
+import { ContactPage } from "./pages/ContactPage"
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/product/:id" element={<ProductPage />} />
           <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </main>
     </BrowserRouter>
