3ea858264b0aadc7479deabb24f64a56bb79f8ca
Patric Angly <padde95@hotmail.se>
2025-05-27 21:45:50 +0200

    Add footer content and styling with responsive grid layout

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,54 @@
-
+import { Link } from "react-router-dom";
+import { RiInstagramFill } from "react-icons/ri";
+import { FaFacebook } from "react-icons/fa";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-[#E9E2DB] pt-16 pb-6 mt-10 grid grid-cols-1 sm:grid-cols-4 h-120 sm:h-90 content-between">
+      <div className="sm:col-start-2 sm:col-end-2">
+        <h2 className="text-2xl font-bold mb-4">Links</h2>
+        <ul>
+          <li>
+            <Link to="/plants">Plants</Link>
+
+            <li>
+              <Link to="/plantcare">Plant care tips</Link>
+            </li>
+            <li>
+              <Link to="/aboutus">About us</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </li>
+        </ul>
+      </div>
+      <div className="sm:col-start-3 sm:col-end-4">
+        <h2 className="text-2xl font-bold mb-4">Follow us</h2>
+        <ul className="flex gap-4 justify-center">
+          <li className="pt-[2px]">
+            <a
+              href="https://www.facebook.com/yourprofile"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook size={36} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/yourprofile"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <RiInstagramFill size={40} />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="col-span-full sm:mt-6 text-xl ">
+        &copy; 2024 Löv. All rights reserved.
+      </div>
+    </div>
+  );
+};
