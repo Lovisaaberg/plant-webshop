99d21ad45930680b515eb73440f6a2bfeccd92be
Lovisa <lovisa.aaberg@gmail.com>
2025-05-29 23:14:27 +0200

    Added images on products, and layout on product cards for mobile

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -6,23 +6,25 @@ export const ProductCard = ({ product }) => {
   const { addToCart } = appContentStore()
 
   return (
-    <div className="flex flex-col items-center gap-4 ">
+    <div className="flex flex-col w-[120px]">
       <Link to={`product/${product.id}`} className="flex flex-col items-start">
         <img
           src={product.image}
           alt="Product Image"
           className="w-[120px] h-[180px] object-cover"
         />
-        <p>{product.name}</p>
-        <p>{product.price}</p>
+        <div className="h-[120px] flex flex-col justify-between mt-[12px] mb-[16px]">
+          <p className="text-left font-semibold text-[18px]">{product.name}</p>
+          <p className="text-left text-[20px]">{product.price} kr</p>
+        </div>
       </Link>
 
-      <div className="flex gap-2 ml-auto">
+      <div className="flex ml-auto mb-[60px]">
         <Button>
           <img
             src="/icons/heart-mobile-menu.png"
             alt="Add to favorites"
-            className="w-8 h-8"
+            className="w-[30px] h-[30px]"
           />
         </Button>
 
@@ -30,7 +32,7 @@ export const ProductCard = ({ product }) => {
           <img
             src="/icons/shopping-cart-plus.png"
             alt="Add to shopping cart"
-            className="w-8 h-8"
+            className="w-[30px] h-[30px] ml-[6px]"
           />
         </Button>
         <Link to={`product/${product.id}`}></Link>
--- client/src/components/ProductsSection.jsx
+++ client/src/components/ProductsSection.jsx
@@ -1,17 +1,20 @@
 import { ProductCard } from "./ProductCard"
 import { products } from "../productData"
 
-const firstProducts = products.slice(0, 4)
+const firstProducts = products
 
 export const ProductsSection = () => {
   return (
-    <div>
-      Our products
-      <div className="grid grid-cols-2 gap-6">
+    <section className="mt-[68px]">
+      <h2 className="text-[28px] md:text-[40px] font-semibold mb-[30px]">
+        Our Plants
+      </h2>
+
+      <div className="grid grid-cols-2 mx-auto max-w-[260px] gap-[60px_60px]">
         {firstProducts.map((product) => (
           <ProductCard product={product} key={product.id} />
         ))}
       </div>
-    </div>
+    </section>
   )
 }
