139805abf24c57bf839d3f782d1d3fe106ad13d7
Tejpex <equis_angel@hotmail.com>
2025-06-23 12:53:20 +0200

    Remove checkout-page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { LandingPage } from "./pages/LandingPage"
 import { PlantsPage } from "./pages/PlantsPage"
 import { ProductPage } from "./pages/ProductPage"
-import { CheckoutPage } from "./pages/CheckoutPage"
 import { ContactPage } from "./pages/ContactPage"
 import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
@@ -20,7 +19,6 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/products" element={<PlantsPage />} />
           <Route path="/product/:id" element={<ProductPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/order" element={<OrderPage />} />
         </Routes>
--- client/src/components/CartPanel.jsx
+++ client/src/components/CartPanel.jsx
@@ -1,11 +1,41 @@
-import { Link } from "react-router-dom"
+import { loadStripe } from "@stripe/stripe-js"
 import { useEffect, useRef } from "react"
+import { appContentStore } from "../stores/appContentStore"
 import { Cart } from "./Cart"
 import { Button } from "./Button"
 import Cross from "/icons/cross.png"
 
 export const CartPanel = ({ isOpen, onClose }) => {
+  const cart = appContentStore((state) => state.cart)
   const panelRef = useRef()
+
+  const handleCheckout = async () => {
+    const response = await fetch(
+      "http://localhost:3000/api/stripe/create-checkout-session",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ products: cart }),
+      }
+    )
+
+    const session = await response.json()
+
+    const stripe = await loadStripe(
+      "pk_test_51RQ74HPTeALF4muskKk4SWJ7tHeeQthYvn68413CMFWtf21Fp9Nwc7y1TOMwZE3wvZrR4nouai1XTYBwNhAaffl200TiEo2T5z"
+    )
+
+    const { error } = await stripe.redirectToCheckout({
+      sessionId: session.id,
+    })
+
+    if (error) {
+      console.error("Error:", error)
+    }
+    console.log(response)
+  }
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -49,37 +79,46 @@ export const CartPanel = ({ isOpen, onClose }) => {
     >
       <div className="flex items-center justify-between py-10 w-full md:w-90 lg:w-120">
         <div className="w-18 md:w-0"></div>
-        <h2 className="font-medium md:font-bold text-xl md:text-3xl lg:text-[40px]">SHOPPING CART</h2>
-        <button onClick={onClose} className="w-8 h-8 md:w-12 md:h-12 mr-10 md:mr-0">
+        <h2 className="font-medium md:font-bold text-xl md:text-3xl lg:text-[40px]">
+          SHOPPING CART
+        </h2>
+        <button
+          onClick={onClose}
+          className="w-8 h-8 md:w-12 md:h-12 mr-10 md:mr-0"
+        >
           <img src={Cross} alt="Close-symbol" />
         </button>
       </div>
       <div className="md:px-15">
         <Cart />
       </div>
-      <Link to="/checkout">
-        <Button
-          text="Go to checkout"
-          className="
-            w-65
-            md:w-90
-            lg:w-120
-            h-15 
-            bg-[#2C7E62] 
-            text-white 
-            rounded-[10px] 
-            shadow-md 
-            mx-15
-            mt-[24px]
-            md:mt-0
-            self-center
-            hover:bg-[#00583A]
-            font-quicksand
-            font-bold
-            text-xl
-            "
-        />
-      </Link>
+      <Button
+        func={handleCheckout}
+        disabled={cart.length === 0}
+        text="Go to checkout"
+        className="
+          w-65
+          md:w-90
+          lg:w-120
+          h-10
+          md:h-12
+          lg:h-15 
+          bg-[#2C7E62] 
+          text-white 
+          rounded-[10px] 
+          shadow-md 
+          mx-15
+          mt-[24px]
+          md:mt-0
+          self-center
+          hover:bg-[#00583A]
+          font-quicksand
+          font-bold
+          text-xl
+          lg:text-2xl
+          lg:py-2
+          "
+      />
       <button
         className="
           text-lg 
