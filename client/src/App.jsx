554e2db58ac7ce1b321518c93000f224cc87e669
Tejpex <equis_angel@hotmail.com>
2025-06-24 15:31:26 +0200

    Add plantcare-page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+// Importing pages and components
 import { LandingPage } from "./pages/LandingPage"
 import { PlantsPage } from "./pages/PlantsPage"
 import { ProductPage } from "./pages/ProductPage"
+import { PlantcarePage } from "./pages/PlantcarePage"
 import { ContactPage } from "./pages/ContactPage"
+import { OrderPage } from "./pages/OrderPage"
 import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
-import "./App.css"
-import { OrderPage } from "./pages/OrderPage"
 import { ScrollToTop } from "./components/ScrollToTop"
+import "./App.css"
 
 function App() {
   return (
@@ -19,6 +22,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/products" element={<PlantsPage />} />
           <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/plantcare" element={<PlantcarePage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/order" element={<OrderPage />} />
         </Routes>
--- client/src/pages/PlantsPage.jsx
+++ client/src/pages/PlantsPage.jsx
@@ -1,8 +1,10 @@
 import { ProductsSection } from "../components/ProductsSection"
+import { NavigationRow } from "../components/NavigationRow"
 
 export const PlantsPage = () => {
   return (
     <div>
+      <NavigationRow links={[{ text: "Plants", to: "/products" }]} />
       <ProductsSection />
     </div>
   )
