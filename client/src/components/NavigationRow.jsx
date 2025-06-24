65c66ea98158470e65b4dcd09b7773c4effd8f93
Tejpex <equis_angel@hotmail.com>
2025-06-24 15:49:15 +0200

    Style navigation-row + contact-page

--- client/src/components/NavigationRow.jsx
+++ client/src/components/NavigationRow.jsx
@@ -3,17 +3,18 @@ import House from "/icons/house.svg"
 
 export const NavigationRow = ({links}) => {
   return (
-    <div className="w-full flex justify-start ml-25 mt-6 mb-10">
+    <div className="w-full flex justify-start items-center ml-10 md:ml-25 mt-6 mb-10">
       <Link to="/">
-        <img src={House} alt="Home icon" className="pr-1 w-6 h-6"/>
+        <img src={House} alt="Home icon" className="pr-1 w-5 h-5 md:w-7 md:h-7" />
       </Link>
       {links.map((link, index) => (
         <Link to={link.to} key={index}>
-          <p>  
+          <p className="font-quicksand font-medium text-base md:text-xl md:font-normal">
             /{link.text}
           </p>
-        </Link>              
+        </Link>
       ))}
+      <p className="font-quicksand font-medium text-base md:text-xl md:font-normal">/</p>
     </div>
   )
 }
